d6b33927bb74bf2c73082d857ed0e273aeef9eb6
abdelkader <abdelkader.lakhdar@epitech.eu>
2026-03-17 19:20:54 +0100

    Récupération de la température ok

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ const result = document.getElementById("result");
 function fetched(){
     console.log("Fetched brr brr");
     const name = city.value;
-    // fetch("https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m")
     fetch ("https://geocoding-api.open-meteo.com/v1/search?name="+name)
     .then(latlon => latlon.json())
     .then(latlon => { 
         const lat = latlon.results[0].latitude
         const lon = latlon.results[0].longitude
-        document.getElementById("result").value = "latitude : " +lat + " || longitude :" + lon ;
+        document.getElementById("result1").value = "latitude : " +lat ;
+         document.getElementById("result2").value = " longitude :" + lon ;
+        
+        return fetch("https://api.open-meteo.com/v1/forecast?latitude="+lat+"&longitude="+lon+"&current=temperature_2m");
     })
-
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.current.temperature_2m)
+        document.getElementById("result3").value = "Temperature  : " +data.current.temperature_2m;
+            
+    });
 
 }
 
